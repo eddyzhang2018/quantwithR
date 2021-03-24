92935082651d9ccffd06785e51cf5f7a97dcec94
eddyzhang2018 <60183020+eddyzhang2018@users.noreply.github.com>
2021-03-25 01:30:02 +1100

    Update Day 1: Quartiles.r

--- Hackerrank_with_R/Statistics/Day 1: Quartiles.r
+++ Hackerrank_with_R/Statistics/Day 1: Quartiles.r
@@ -1,1 +1,16 @@
+# Problem Statement: https://www.hackerrank.com/challenges/s10-quartiles/problem
+# Difficulty: Easy
+# Max Score: 30
 
+ipt <- sort(scan("stdin")[-1])
+
+len_ipt <- length(ipt)
+
+lower_bound <- floor(len_ipt/2)
+upper_bound <- ceiling(len_ipt/2) + 1
+
+q1 <- median(ipt[1:lower_bound])
+q2 <- median(ipt)
+q3 <- median(ipt[upper_bound:len_ipt])
+
+cat(q1, q2, q3, sep = "\n")
